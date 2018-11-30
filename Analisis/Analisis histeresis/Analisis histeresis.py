7b7b806da5e39cba4640857c80d582b2a0b5b608
tomasferreirachase <tomaschase96@gmail.com>
2018-11-30 11:22:09 -0300

    .

--- Analisis/Analisis histeresis/Analisis histeresis.py
+++ Analisis/Analisis histeresis/Analisis histeresis.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 import Funciones as f
 
 
-path = 'C:/Users/LINE/Desktop/Finazzi-Ferreira/Labo-7/Analisis/Analisis histeresis/Mediciones histeresis/results led PID/0-20mA, step 25 uA, wait 10ms, bis bis/iv/1 (iv).txt'
+path = '/home/tomas/Desktop/Labo 6 y 7/Labo-7/Analisis/Analisis histeresis/Mediciones histeresis/results led 0-20/Estacionario 6/iv/6 (iv).txt'
 
 data = np.loadtxt(path)
 I_sipm = data[:, 0]
@@ -35,13 +35,13 @@ plt.errorbar(1000*I_led[int(len(I_sipm)/2):-1], 1000*I_sipm[int(len(I_sipm)/2):-
                    fmt='.r', capsize = 1, label = 'Vuelta')
 #plt.plot(I_led[1:int(len(I_sipm)/2)], I_sipm[1:int(len(I_sipm)/2)], '.', label = 'Ida')
 #plt.plot(I_led[int(len(I_sipm)/2):-1], I_sipm[int(len(I_sipm)/2):-1], '.', label = 'Vuelta')
-plt.xlabel(r'$I_{led} (mA)$', size = 15)
-plt.ylabel(r'$I_{sipm} (mA)$', size = 15)
+plt.xlabel(r'$I_{led} (mA)$', size = 22)
+plt.ylabel(r'$I_{sipm} (mA)$', size = 22)
+plt.tick_params(labelsize=22)
 plt.legend()
 plt.grid(True)
 plt.yscale('log')
 plt.xscale('log')
-plt.scatter(11.3, 0.355)
 
 I_led_dif, I_sipm_dif = dif(I_led, I_sipm)
 I_sipm_err_dif = [np.sqrt(I_sipm_err[1:int(len(I_sipm)/2)][i]**2 - I_sipm_err[int(len(I_sipm)/2):-1][i]**2) for i in range(len(I_sipm_err[int(len(I_sipm)/2):-1]))]
@@ -49,17 +49,18 @@ I_sipm_err_dif = [np.sqrt(I_sipm_err[1:int(len(I_sipm)/2)][i]**2 - I_sipm_err[in
 fig, (ax1, ax2) = plt.subplots(2, 1)
 #ax1.plot(I_led_dif, [-i*10**3 for i in I_sipm_dif], 'og')
 ax1.errorbar(I_led_dif, [-i*10**3 for i in I_sipm_dif],xerr=f.error_I(I_led_dif, '2612', source=True), yerr=[i*10**3 for i in I_sipm_err_dif], fmt='og', capsize=3)
-ax1.set_xlabel(r'$I_{led} (mA)$', size = 15)
-ax1.set_ylabel(r'$\Delta I_{sipm} (mA)$', size = 15)
+ax1.set_xlabel(r'$I_{led} (mA)$', size = 20)
+ax1.set_ylabel(r'$\Delta I_{sipm} (mA)$', size = 20)
+ax1.tick_params(labelsize=20)
 ax1.grid(True)
 values, bins = np.histogram(I_sipm_dif, bins = len(I_sipm_dif))
 m, bins, _ = ax2.hist([-i*10**3 for i in I_sipm_dif], bins=10)
 #ax2.plot(bins[:-1], values, lw=2)
-ax2.set_xlabel(r'$\Delta I_{sipm} (mA)$', size = 15)
-ax2.set_ylabel('# Entradas', size = 15)
+ax2.set_xlabel(r'$\Delta I_{sipm} (mA)$', size = 20)
+ax2.set_ylabel('# Entradas', size = 20)
 bins_err = bins + (bins[2] - bins[1])/2
 ax2.errorbar(bins_err[:-1], m, yerr = [np.sqrt(i) for i in m], fmt = '.r', capsize = 3)
-plt.tight_layout()
+ax2.tick_params(labelsize=20)
 #%%
 ''' Simulacion de datos gaussianos y uniformes para comparar con lo medido'''
 
