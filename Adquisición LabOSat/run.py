ded71f19f2f14556865de07c2f9f2bf65d637a79
labosat <43675887+labosat@users.noreply.github.com>
2018-12-26 17:51:35 -0300

    datos

--- Adquisición LabOSat/run.py
+++ Adquisición LabOSat/run.py
@@ -2,7 +2,7 @@ from k2612B import run
 import time
 
 N          = 3
-group_path = 'Encapsulado 1\\5'
+group_path = 'Encapsulado 1\\7'
 test       = ['iv', 'self_heating']
 plotFlag   = 1
 saveFlag   = 1
