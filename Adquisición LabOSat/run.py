4c9aa1d2b71fe1068f75da1042e73d6daa6e2160
labosat <43675887+labosat@users.noreply.github.com>
2018-12-27 14:27:43 -0300

    .

--- Adquisición LabOSat/run.py
+++ Adquisición LabOSat/run.py
@@ -1,8 +1,9 @@
 from k2612B import run
 import time
+import winsound
 
-N          = 3
-group_path = 'Encapsulado 1 tomi\\2'
+N          = 5
+group_path = 'Encapsulado 1 con grasa\\3'
 test       = ['iv', 'self_heating']
 plotFlag   = 1
 saveFlag   = 1
@@ -11,3 +12,7 @@ for i in range(1, N + 1):
     print(str(i))
     run(i, test[0], group_path, plotFlag, saveFlag)
     time.sleep(0)
+
+winsound.Beep(750, 750)
+winsound.Beep(750, 750)
+winsound.Beep(750, 750)
